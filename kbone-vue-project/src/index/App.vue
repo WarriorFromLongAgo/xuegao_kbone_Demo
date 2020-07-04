129f5c70f79f56941f6a55b87d628d14bd96a657
WarriorFromLongAgo <hubeixuegao@gmail.com>
2020-07-04 19:21:04 +0800

    webpack kbone 2

--- kbone-vue-project/src/index/App.vue
+++ kbone-vue-project/src/index/App.vue
@@ -1,13 +1,55 @@
 <template>
-  
+  <div>
+    <div>
+      <ul class="tabbar">
+        <li>
+          <router-link class="link" to="/test/aaa">aaa</router-link>
+        </li>
+        <li>
+          <router-link class="link" to="/test/bbb">bbb</router-link>
+        </li>
+      </ul>
+    </div>
+    <router-view></router-view>
+    <button class="btn" @click="throwError">throw on error</button>
+    <div style="margin-left: 20px">
+      <p>
+        这是
+        <span>1</span>段中间插入了span的文本
+      </p>
+    </div>
+    <div>
+      <p
+        style="width: 400px;"
+      >这段看起来特别特别长的文本，文本宽度是400px，测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</p>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Footer from "../common/Footer.vue";
 export default {
+  components: {
+    Footer
+  },
+  mounted() {
+    console.log(TEST_VAR_STRING);
+    console.log(TEST_VAR_NUMBER);
 
-}
+    console.log("before = ", document.cookie);
+    document.cookie =
+      "time=${+new Date()}; expires=Wed Jan 01 2220 00:00:00 GMT+0800; path=/;";
+    console.log("after = ", document.cookie);
+  },
+  methods: {
+    throwError() {
+      setTimeout(() => {
+        throw new Error("Iam on error");
+      }, 0);
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style lang="stylus" scoped></style>
--- kbone-vue-project/src/index/main.mp.js
+++ kbone-vue-project/src/index/main.mp.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+// 路由
+import Router from 'vue-router'
 // 不需要.vue的原因，webpack里面做了配置
 import App from './App'
+import AAA from './AAA.vue'
+import BBB from './BBB.vue'
 
 // 通过 export default 对外暴露函数
 // 每个小程序的页面编译入口，都需要创建一个sj文件，都要去创建 createApp 函数
@@ -9,9 +13,47 @@ export default function createApp() {
     container.id = 'app'
     document.body.appendChild(container)
 
+    window.onload = function () {
+        // 自动获取wx手机页面的宽度
+        document.documentElement.style.fontSize = wx.getSystemInfoSync().screenWidth / 16 + 'px'
+        // 使用rem的单位
+        document.documentElement.style.backgroundColor = '#fffbe7'
+    }
+
+    // 错误
+    window.onerror = (message, source, lineno, colno, error) => {
+        console.log('window.onerror => ', message, source, lineno, colno, error)
+    }
+    window.addEventListener('error', evt => console.log('error'))
+
+    // 载入插件
+    Vue.use(Router)
+
+    // router: {
+    // 多个路由，一个页面显示多个路由的信息，，用于页面的跳转
+    // index: [
+    // entry 作为默认显示哪一个
+    // '/test/aaa',
+    // '/test/bbb'
+    // ],
+    const router = new Router({
+        mode: 'history',
+        routerArr: [
+            {
+                path: '/test/aaa',
+                component: AAA
+            }, {
+                path: '/test/bbb',
+                component: BBB
+            }
+        ]
+    })
+
     return new Vue({
         el: '#app',
+        // 路由的装载
+        router: router,
         render: h => h(App)
-    }) 
-    
+    })
+
 }
